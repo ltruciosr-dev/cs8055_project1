ed40b7c91826f3b06ab97aeb4ff96093546b8222
Luis Enrique Trucios Ruiz <luis.trucios@tenpo.cl>
2024-05-09 00:57:19 -0500

    agrupar los testing

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyke import knowledge_engine,krb_traceback, goal
+import driver
+
+''' Relacion entre platos e ingredientes '''
+# driver.test_ingredientes()
+# driver.test_plato_ingredientes()
+# driver.test_plato_tipos()
+# driver.test_plato_nutrientes()
+
+''' Relacion entre platos y dieta '''
+# driver.test_plato_valor_nutricional()
+# driver.test_plato_dieta()
+
+''' Relacion entre restaurantes y platos '''
+driver.test_restaurante()
+# driver.test_dieta_restaurante_posibles_platos_menu()
+
+# driver.test_questions()
